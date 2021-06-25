419a5510359d0191c5bc0c3018f19c5832018759
Михаил Проказин <bearpro@outlook.com>
2021-06-25 20:05:28 +0300

    Добавлен файл решения Visual Studio

---
+++ Server/Controllers/FibonacciController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+        private readonly ILogger<FibonacciController> _logger;
+        private readonly IBus bus;
+
+        public FibonacciController(ILogger<FibonacciController> logger, IBus bus)
+        {
+            _logger = logger;
+            this.bus = bus;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(500, "Client must do POST request.");
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromQuery] int n)
+        {
+            return StatusCode(200, $"fib({n})");
+        }
+    }
+}
